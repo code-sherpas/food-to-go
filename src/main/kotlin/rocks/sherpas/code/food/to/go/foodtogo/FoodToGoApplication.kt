2a06370ee9619a4047340d7c3227db8c6b5b74b5
Domingo <domingo.espinosa@code.seat>
2022-02-10 12:10:52 +0100

    first commit

---
+++ src/main/kotlin/rocks/sherpas/code/food/to/go/foodtogo/FoodToGoApplication.kt
@@ -0,0 +1,11 @@
+package rocks.sherpas.code.food.to.go.foodtogo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FoodToGoApplication
+
+fun main(args: Array<String>) {
+	runApplication<FoodToGoApplication>(*args)
+}
---
+++ src/test/kotlin/rocks/sherpas/code/food/to/go/foodtogo/FoodToGoApplicationTests.kt
@@ -0,0 +1,13 @@
+package rocks.sherpas.code.food.to.go.foodtogo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FoodToGoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
